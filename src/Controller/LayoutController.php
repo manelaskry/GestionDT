c1a8a6dc0cfeab189f2310f6342dc08b82fe06e1
Oumaimabben <obenothmen865@gmail.com>
2023-11-20 15:34:54 +0100

    front

--- src/Controller/LayoutController.php
+++ src/Controller/LayoutController.php
@@ -15,4 +15,11 @@ class LayoutController extends AbstractController
             'controller_name' => 'LayoutController',
         ]);
     }
+    #[Route('/tableau', name: 'app_tableau')]
+    public function tab(): Response
+    {
+        return $this->render('layout/tableau.html.twig', [
+            'controller_name' => 'LayoutController',
+        ]);
+    }
 }
